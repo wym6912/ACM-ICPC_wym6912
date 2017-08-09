23d2290c82d77605146e593b7374961481e02ee8
wym6912 <wym6912@outlook.com>
2017-08-09 16:46:13 +0800

    Upload LightOJ 1070
    Using a NEW OJ!

---
+++ LightOJ/1000.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, T;
+    cin >> T;
+    for(int aa = 1;aa <= T;aa ++)
+    {
+        cin >> a >> b;
+        cout << "Case " << aa << ": " << a + b << endl;
+    }
+    return 0;
+}
