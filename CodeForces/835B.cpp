6e56b9070a83de040427588073f898aed106b843
wym6912 <wym6912@outlook.com>
2017-08-16 16:23:51 +0800

    Update CodeForces 835B

--- CodeForces/835B.cpp
+++ CodeForces/835B.cpp
@@ -2,19 +2,37 @@
 
 using namespace std;
 
+int x[11];
+
 int main()
 {
-	int n,ppp,x,sum;
+	int n,i,sum,len,e,ans,tmp;
 	while(~ scanf("%d",&n))
 	{
-		//ppp = 1;
+		len = 1;
 		sum = 0;
-		while(~ scanf("%1d",&x))
-			sum += x;
-		ppp = n - sum;
-		sum = 0;
-		while(ppp > 0){ppp /= 10;sum ++;}
-		printf("%d\n",sum);
+		ans = 0;
+		memset(x,0,sizeof(x));
+		while(~ scanf("%1d",&tmp))
+			sum += tmp, x[tmp] ++;
+		if(sum >= n)
+		{
+			printf("0\n");
+			continue;
+		}
+		for(int i = 0;i <= 9;i ++)
+			if(sum >= n)break;
+			else 
+			{
+				tmp = 0;
+				while(sum < n && tmp < x[i])
+				{
+					sum += 9 - i;
+					ans ++;
+					tmp ++;
+				}
+			}
+		printf("%d\n", ans);
 	}
 	return 0;
 }
