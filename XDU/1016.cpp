3a7968d809187000e280c183deaa4e904a2bea86
wym6912 <wym6912@outlook.com>
2017-09-06 20:14:34 +0800

    Upload XDOJ 1016 & 1053
    These are easy questions.

---
+++ XDU/1016.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() 
+{
+    long long x, y;
+    while (~ scanf ("%lld%lld", &x, &y))printf("%lld\n", x + y);
+    return 0;
+ 
+}
+ 
+/**************************************************************
+    Problem: 1016
+    User: 16020120071
+    Language: C++
+    Result: ÕýÈ· 
+    Time:1 ms
+    Memory:1080 kb
+****************************************************************/
---
+++ XDU/1053.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+ 
+using namespace std;
+ 
+int main()
+{
+    int x;
+    while(~ scanf("%d",&x))
+    {
+        if(x > 0)puts("yes");
+        else if(x == 0)puts("light");
+        else puts("no");
+    }
+    return 0;
+}
+/**************************************************************
+    Problem: 1053
+    User: 16020120071
+    Language: C++
+    Result: ÕýÈ·
+    Time:0 ms
+    Memory:1080 kb
+****************************************************************/
