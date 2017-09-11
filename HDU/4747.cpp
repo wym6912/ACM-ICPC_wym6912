f4912ccb4438ec0a5d8f000559ccb9d1a1066482
wym6912 <wym6912@outlook.com>
2017-09-11 20:48:18 +0800

    Upload HDU 4747 which is easier to understand

--- HDU/4747.cpp
+++ HDU/4747.cpp
@@ -1,148 +1,120 @@
-
-#pragma comment(linker, "/STACK:102400000,102400000")  
-#include <iostream>  
-#include <cstring>  
-#include <cstdlib>  
-#include <cstdio>  
-#include <queue>  
-#include <vector>  
-#include <algorithm>  
-#include <cmath>  
-#include <map>  
-#include <set>  
-#include <stack>  
-#include <string.h>  
-//freopen ("in.txt" , "r" , stdin);  
-using namespace std;  
-  
-#define eps 1e-8  
-#define zero(_) (abs(_)<=eps)  
-const double pi=acos(-1.0);  
-typedef long long LL;  
-const int Max=200010;  
-const LL INF=0x3FFFFFFF;  
-int arr[Max];  
-int mex[Max];  
-map<int,int> maps;  
-struct node  
-{  
-    LL sum;//表示和  
-    int ma;//表示区间最大值  
-    int lazy;//表示区间值一样  
-    int l,r;  
-    node* pl,*pr;  
-} tree[3*Max];  
-int tot=0;  
-int Next[Max];  
-void build(node* p,int left,int right)  
-{  
-    p->l=left;  
-    p->r=right;  
-    if(left==right)  
-    {  
-        p->ma=mex[left];  
-        p->sum=mex[left];  
-        p->lazy=0;  
-        return ;  
-    }  
-    int middle=(left+right)/2;  
-    tot++;  
-    p->pl=tree+tot;  
-    build(p->pl,left,middle);  
-    tot++;  
-    p->pr=tree+tot;  
-    build(p->pr,middle+1,right);  
-    p->sum=p->pl->sum+p->pr->sum;  
-    p->ma=max(p->pl->ma,p->pr->ma);  
-}  
-  
-void update(node* p,int value,int left,int right);  
-void down(node* p)  
-{  
-    if(p->l==p->r)  
-        return ;  
-    p->lazy=0;  
-    int middle=(p->l+p->r)/2;  
-    update(p->pl,p->ma,p->l,middle);  
-    update(p->pr,p->ma,middle+1,p->r);  
-}  
-int findpos(node* p,int value)  
-{  
-    if(p->l==p->r)  
-    {  
-        if(p->ma>=value)  
-            return p->l;  
-    }  
-    if(p->lazy)  
-        down(p);  
-    if(p->pl->ma>value)  
-        return findpos(p->pl,value);  
-    return findpos(p->pr,value);  
-}  
-void update(node* p,int value,int left,int right)  
-{  
-    if(p->l==left&&p->r==right)  
-    {  
-        p->lazy=1;  
-        p->ma=value;  
-        p->sum=(p->r-p->l+1)*value;  
-        return;  
-    }  
-    LL ans=0;  
-    int middle=(p->l+p->r)/2;  
-    if(p->lazy)  
-    {  
-        down(p);  
-    }  
-    if(left>middle)  
-    {  
-        update(p->pr,value,left,right);  
-    }  
-    else if(right<=middle)  
-        update(p->pl,value,left,right);  
-    else  
-    {  
-        update(p->pl,value,left,middle);  
-        update(p->pr,value,middle+1,right);  
-    }  
-    p->sum=p->pl->sum+p->pr->sum;  
-    p->ma=max(p->pl->ma,p->pr->ma);  
-}  
-int n;  
-int main()  
-{  
-  
-    while(cin >> n && n)  
-    {  
-        tot=0;  
-        memset(tree,0,sizeof(tree));  
-        memset(Next,0,sizeof(Next));  
-        maps.clear();  
-        for(int i=1; i<=n; i++)  
-            scanf("%d",arr+i);  
-        int p=0;  
-        for(int i=1; i<=n; i++)  
-        {  
-            Next[maps[arr[i]]]=i;  
-            maps[arr[i]]=i;  
-            while(maps.find(p)!=maps.end())p++;  
-            mex[i]=p;  
-        }  
-        build(tree,1,n);  
-        LL ans=0;  
-        for(int i=1; i<=n; i++)  
-        {  
-            ans+=tree->sum;  
-            if(Next[i]==0) Next[i]=n+1;  
-            if(tree->ma>arr[i])  
-            {  
-                int pos=findpos(tree,arr[i]);  
-                if(pos<=Next[i]-1)  
-                    update(tree,arr[i],pos,Next[i]-1);  
-            }  
-            update(tree,0,i,i);  
-        }  
-        cout<<ans<<"\n";  
-    }  
-    return 0;  
-}  
+# include<bits/stdc++.h>
+using namespace std;
+# define LL long long
+# define mid (l+(r-l)/2)
+ 
+const int N=200000;
+ 
+int a[N+5];
+int mex[N+5];
+int nxt[N+5];
+LL tr[N*4+5];
+LL lazy[N*4+5];
+int maxn[N*4+5];
+ 
+inline void getMex(int n)
+{
+    map<int,int>mp;
+    map<int,int>f;
+    fill(nxt,nxt+1+n,n+1);
+    int m=0;
+    for(int i=1;i<=n;++i){
+        mp[a[i]]=1;
+        while(mp[m]) ++m;
+        mex[i]=m;
+        if(f[a[i]])
+            nxt[f[a[i]]]=i;
+        f[a[i]]=i;
+    }
+}
+ 
+inline void pushUp(int rt)
+{
+    tr[rt]=tr[rt<<1]+tr[rt<<1|1];
+    maxn[rt]=max(maxn[rt<<1],maxn[rt<<1|1]);
+}
+ 
+inline void pushDown(int rt,int l,int r)
+{
+    if(lazy[rt]!=-1){
+        lazy[rt<<1]=lazy[rt<<1|1]=lazy[rt];
+        maxn[rt<<1]=maxn[rt<<1|1]=lazy[rt];
+        tr[rt<<1]=lazy[rt]*(LL)(mid-l+1);
+        tr[rt<<1|1]=lazy[rt]*(LL)(r-mid);
+        lazy[rt]=-1;
+    }
+}
+ 
+inline void build(int rt,int l,int r)
+{
+    lazy[rt]=-1;
+    if(l==r){
+        tr[rt]=mex[l];
+        maxn[rt]=mex[l];
+    }else{
+        build(rt<<1,l,mid);
+        build(rt<<1|1,mid+1,r);
+        pushUp(rt);
+    }
+}
+ 
+inline int query_up(int rt,int l,int r,int L,int R,int x)
+{
+    if(l==r){
+        if(tr[rt]<x) return 0;
+        return l;
+    }
+    pushDown(rt,l,r);
+    if(L<=mid&&x<maxn[rt<<1]){
+        int k=query_up(rt<<1,l,mid,L,R,x);
+        if(k>0) return k;
+    }
+    if(R>mid&&x<maxn[rt<<1|1]){
+        int k=query_up(rt<<1|1,mid+1,r,L,R,x);
+        if(k>0) return k;
+    }
+    return 0;
+}
+ 
+inline void update(int rt,int l,int r,int L,int R,int x)
+{
+    if(L<=l&&r<=R){
+        lazy[rt]=x;
+        tr[rt]=(LL)x*(LL)(r-l+1);
+        maxn[rt]=x;
+    }else{
+        pushDown(rt,l,r);
+        if(L<=mid) update(rt<<1,l,mid,L,R,x);
+        if(R>mid) update(rt<<1|1,mid+1,r,L,R,x);
+        pushUp(rt);
+    }
+}
+ 
+inline LL query(int rt,int l,int r,int L,int R)
+{
+    if(L<=l&&r<=R) return tr[rt];
+    pushDown(rt,l,r);
+    LL res=0;
+    if(L<=mid) res+=query(rt<<1,l,mid,L,R);
+    if(R>mid) res+=query(rt<<1|1,mid+1,r,L,R);
+    return res;
+}
+ 
+int main()
+{
+    int n;
+    while(scanf("%d",&n)&&n)
+    {
+        for(int i=1;i<=n;++i) scanf("%d",a+i);
+        getMex(n);
+        build(1,1,n);
+        LL ans=query(1,1,n,1,n);
+        for(int i=1;i<n;++i){
+            int p=query_up(1,1,n,i+1,nxt[i]-1,a[i]);
+            if(p>0) update(1,1,n,p,nxt[i]-1,a[i]);
+            ans+=query(1,1,n,i+1,n);
+        }
+        printf("%lld\n",ans);
+    }
+    return 0;
+}
