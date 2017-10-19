a434ed29ded9f35b79c4a0b48d7b83084f4bad46
wym6912 <wym6912@outlook.com>
2017-10-19 13:22:51 +0800

    Upload CodeForces 876D

---
+++ CodeForces/876D.cpp
@@ -0,0 +1,65 @@
+//#define NOSTDCPP
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+
+#endif
+
+#define RESET(_) memset(_, 0, sizeof(_))
+#define RESET_(_, val) memset(_, val, sizeof(_))
+#define mp make_pair
+#define fi first
+#define se second
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+
+const int MOD = 1e9 + 7;
+const int maxn = 300009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+bool f[maxn];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, x, tot;
+	while(cin >> n)
+	{
+		cout << 1 << ' ' ;
+		tot = 0;
+		int end = n;
+		for(int i = 1;i <= n;i ++)
+		{
+			cin >> x;
+			f[x] = true, tot ++;
+			while(f[end])end --, tot --;
+			cout << tot + 1 << ' ';
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
