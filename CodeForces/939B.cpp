664410eeba623d07f9d73a7693d2271b5fc5b34c
wym6912 <wym6912@outlook.com>
2018-02-18 16:24:24 +0800

    Upload CodeForces 939A & 939B & 939D & 939E
    This Round is an another pity round.
    The Problem D is MST problem but I can't find it.
    And Problem E is Divide and Conquer Problem, but my code get something wrong.

---
+++ CodeForces/939B.cpp
@@ -0,0 +1,131 @@
+//#define NOSTDCPP
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 300009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+ll data[maxn];
+ll sum;
+int n;
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	while(cin >> sum >> n)
+	{
+		int ans = 1;
+		ll len = 1, delta = 0, ansl = -1, ansdel = 1e18;
+		for(int i = 1; i <= n; i ++)
+		{
+			cin >> data[i];
+			len = sum / data[i];
+			delta = sum - len * data[i];
+			if(delta < ansdel)
+			{
+				ansdel = delta;
+				ansl = len;
+				ans = i;
+			}
+		}
+		cout << ans << ' ' << ansl << endl;
+	}
+	return 0;
+}
+
---
+++ CodeForces/939E.cpp
@@ -0,0 +1,168 @@
+//#define NOSTDCPP
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 500009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+ll data[maxn], pre[maxn];
+int q, p;
+
+int mid, mmid;
+
+double f(int x)
+{
+	if(x != p)
+	{
+		double ans = (double)data[p];
+		ans = ans + (double)pre[x];
+		return (double)(ans / (double(x + 1)));
+	}
+	else return double((double)(pre[p]) / (double)(x));
+}
+
+double ans()
+{
+	int L = 1, R = p - 1;
+	mid = L + (R - L) / 3, mmid = R - (R - L) / 3;
+	for(int i = 1; i <= 200 && L < R; i ++)
+	{
+		if(f(mid) <= f(mmid))
+			R = mmid; 
+		else 
+			L = mid; 
+		mid = L + (R - L) / 3;
+		mmid = R - (R - L) / 3; 
+	}
+	//cout << L << ' ' << R << ' ' << mid << ' ' << mmid << endl;
+	return min(f((L + R) / 2), min(f((L + R) / 2 - 1), f((L + R) / 2 + 1)));
+	return f(mid);
+}
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	ll d;
+	while(scan_d(q))
+	{
+		p = 0;
+		pre[0] = 0;
+		while(q --)
+		{
+			scan_d(t);
+			switch(t)
+			{
+				case 1:
+					scan_d(d);
+					p ++;
+					data[p] = d;
+					pre[p] = pre[p - 1] + data[p];
+					break;
+				case 2:
+					printf("%.15f\n", (double)data[p] - ans());
+					break;
+				default:
+					assert(false);
+			}
+		}
+	}
+	return 0;
+}
+
