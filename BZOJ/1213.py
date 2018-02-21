7407d609c7292e411c5d375a1e369c6cc3643750
wym6912 <wym6912@outlook.com>
2018-02-21 11:56:31 +0800

    Upload BZOJ 1213 Using Python

---
+++ BZOJ/1213.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    m, n = int(input()), int(input())
+    l, r = 0, 1
+    while r ** m <= n: l = r; r = r * 2
+    while l + 1 < r:
+        mid = ( l + r ) // 2
+        if mid ** m <= n:
+            l = mid
+        else:
+            r = mid
+    if r ** m <= n:
+        print (r)
+    else:
+        print (l)
