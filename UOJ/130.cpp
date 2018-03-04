11710dbe4f014bb21d324f878ce2700027bb589b
wym6912 <wym6912@outlook.com>
2018-03-04 21:48:47 +0800

    Upload BZOJ 4198 & UOJ 130

---
+++ UOJ/130.cpp
@@ -0,0 +1,163 @@
+//#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+using namespace std;
+
+typedef unsigned long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 300009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+typedef struct node
+{
+	int mergen;
+	ll data;
+	node(int mergen, ll data) : mergen(mergen), data(data){}
+	bool operator < (const node &b) const
+	{
+		return data > b.data || (data == b.data && mergen > b.mergen);
+	}
+}node;
+
+priority_queue <node> pq;
+ll data[100];
+
+int main()
+{
+	int n, k, mk;
+	ll ans = 0, tmp;
+	scan_d(n);
+	scan_d(k);
+	node p(0, 1), zero(0, 0);
+	for(int i = 1; i <= n; i ++)
+	{
+		scan_d(p.data);
+		pq.push(p);
+	}
+	while((n - 1) % (k - 1)) //错在这里了 
+	{
+		n ++;
+		pq.push(zero);
+	}
+	while(pq.size() != 1)
+	{
+		mk = 0;
+		tmp = 0;
+		for(int i = 1; i <= k; i ++)
+		{
+			p = pq.top();
+			pq.pop();
+			tmp += p.data;
+			mk = max(mk, p.mergen);
+		}
+		p.data = tmp;
+		p.mergen = mk + 1;
+		ans += p.data;
+		pq.push(p);
+	}
+	out_number(ans);
+	puts("");
+	out_number(p.mergen);
+	puts("");
+	return 0;
+}
+
