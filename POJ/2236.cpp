ce01a77376a9da55f2017e49f199bff3489ebca7
wym6912 <wym6912@outlook.com>
2018-04-17 21:43:25 +0800

    Upload POJ 2236

---
+++ POJ/2236.cpp
@@ -0,0 +1,176 @@
+#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 1009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#define cin.tie(0); cin.tie(nullptr);
+#define cout.tie(0); cout.tie(nullptr);
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+struct node
+{
+	double x, y;
+}data[maxn];
+
+double dis(node a, node b)
+{
+	return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
+
+int father[maxn];
+int find(int x){return x == father[x] ? x : father[x] = find(father[x]);}
+void merge(int x, int y)
+{
+	x = find(x), y = find(y);
+	if(x != y)
+		father[x] = y;
+}
+
+double cost[maxn][maxn];
+
+bool fixe[maxn];
+
+int n, f, t;
+double d;
+char ch;
+
+int main()
+{
+	scanf("%d %lf", &n, &d);
+	for(int i = 1; i <= n; i ++)
+	{
+		scanf("%lf%lf", &data[i].x, &data[i].y);
+		for(int j = 1; j < n; j ++)
+			cost[i][j] = cost[j][i] = dis(data[i], data[j]);
+	}
+	for(int i = 1; i <= n; i ++)
+		father[i] = i;
+	while(scanf("\n%c", &ch) == 1)
+	{
+		switch(ch)
+		{
+			case 'S':
+				scanf("%d %d", &f, &t);
+				find(f) == find(t) && fixe[f] && fixe[t]
+				 ? puts("SUCCESS") : puts("FAIL");
+				break;
+			case 'O':
+				scanf("%d", &f);
+				fixe[f] = true;
+				for(int i = 1; i <= n; i ++)
+				{
+					if(cost[i][f] <= d && i != f && fixe[i])
+						merge(i, f);
+				}
+				//for(int i = 1; i <= n; i ++)
+				//	cout << father[i] << ' ';
+				//cout << endl;
+				break;
+			default:
+				assert(ch == 'O' || ch == 'S');
+		}
+	}
+	return 0;
+}
+
