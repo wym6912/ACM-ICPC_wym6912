fcd16287b5687d8183044d317d7b81f2b1a97e15
wym6912 <wym6912@outlook.com>
2018-09-11 17:50:26 +0800

    Update SPOJ LCA

--- SPOJ/LCA.cpp
+++ SPOJ/LCA.cpp
@@ -1,179 +1,221 @@
-//#define NOSTDCPP
+#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
 #ifndef NOSTDCPP
 
 #include <bits/stdc++.h>
 
 #else
 
-#include <cstdio>
-#include <cstring>
-#include <cmath>
-#include <iostream>
 #include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
 #include <map>
-#include <set>
-#include <vector>
-#include <string>
+#include <ostream>
 #include <queue>
+#include <set>
+#include <sstream>
 #include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
 
 #endif
 
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
 # define RESET(_) memset(_, 0, sizeof(_))
 # define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+# define next __Chtholly__
+# define x1 __Mercury__
+# define y1 __bbtl04__
+# define index __ikooo__
 
 using namespace std;
 
 typedef long long ll;
 typedef vector <int> vi;
 typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
 
 const int MOD = 1e9 + 7;
-const int maxn = 10010;
-const int maxm = 300009;
+const int maxn = 3000 + 10;
+const int maxm = 1000 + 10;
 const double pi = acos(-1.0);
 const double eps = 1e-6;
 
-int rmq[2 * maxn];
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
 
-struct ST
+template <class T>
+inline bool scan_d(T & ret)
 {
-	int mm[2 * maxn];
-	int dp[2 * maxn][20];
-	void init(int n)
-	{
-		mm[0] = -1;
-		for(int i = 1;i <= n;i ++)
-		{
-			mm[i] = ((i & (i - 1)) == 0 ? mm[i - 1] + 1 : mm[i - 1]);
-			dp[i][0] = i;
-		}
-		for(int j = 1;j <= mm[n];j ++)
-			for(int i = 1; i + (1 << j) - 1 <= n;i ++)
-				dp[i][j] = rmq[dp[i][j - 1]] < rmq[dp[i + (1 << (j - 1))][j - 1]] ?
-				  dp[i][j - 1] : dp[i + (1 << (j - 1))][j - 1];
-	}
-	int query(int a, int b)
-	{
-		if(a > b)swap(a, b);
-		int k = mm[b - a + 1];
-		return rmq[dp[a][k]] <= rmq[dp[b - (1 << k) + 1][k]] ?
-			dp[a][k] : dp[b - (1 << k) + 1][k];
-	}
-};
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#define cin.tie(0); cin.tie(nullptr);
+#define cout.tie(0); cout.tie(nullptr);
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
 
-struct Edge
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int n;
+
+struct eddy
 {
 	int to, next;
-}edge[2 * maxn];
+}edg_2[maxn];
+int fir[maxm], eddy_tot;
+int deep[maxm], ffather[maxm], son[maxm], size[maxm], head[maxm], out[maxm], ans[maxm];
 
-int tot, head[maxn];
-
-int F[maxn * 2]; // dfs sequence
-int P[maxn]; //p[i] = i in F for the first time
-int cnt;
-ST st;
-
-void init()
+void addedge(int f, int t)
 {
-	tot = 0;
-	RESET_(head, -1);
+	edg_2[++ eddy_tot].to = t;
+	edg_2[eddy_tot].next = fir[f];
+	fir[f] = eddy_tot;
 }
 
-void addedge(int u, int v)
+void dfs1(int u, int pre, int d)
 {
-	edge[tot].to = v;
-	edge[tot].next = head[u];
-	head[u] = tot ++;
-}
-
-void dfs(int u, int pre, int dep)
-{
-	F[++ cnt] = u;
-	rmq[cnt] = dep;
-	P[u] = cnt;
-	for(int i = head[u]; i != -1; i = edge[i].next)
+	deep[u] = d;
+	ffather[u] = pre;
+	son[u] = 0;
+	size[u] = 1;
+	for(int i = fir[u]; i; i = edg_2[i].next)
 	{
-		int v = edge[i].to;
-		if(v == pre)continue;
-		dfs(v, u, dep + 1);
-		F[++ cnt] = u;
-		rmq[cnt] = dep;
+		if(edg_2[i].to != pre)
+		{
+			dfs1(edg_2[i].to, u, d + 1);
+			size[u] += size[edg_2[i].to];
+			if(! son[u] || size[edg_2[i].to] > size[son[u]])
+				son[u] = edg_2[i].to;
+		}
 	}
 }
 
-void LCA_init(int root, int node_num)
+void getpos(int u, int sp)
 {
-	cnt = 0;
-	dfs(root, root, 0);
-	st.init(2 * node_num - 1);
+	head[u] = sp;
+	if(son[u]) getpos(son[u], sp);
+	for(int i = fir[u]; i; i = edg_2[i].next)
+	{
+		if(edg_2[i].to != son[u] && edg_2[i].to != ffather[u])
+			getpos(edg_2[i].to, edg_2[i].to); 
+	}
 }
 
-int query_LCA(int u, int v)
+int LCA(int u, int v)
 {
-	return F[st.query(P[u], P[v])];
+    int f1 = head[u], f2 = head[v];
+    while(f1 != f2)
+    {
+        if(deep[f1] < deep[f2])
+        {
+            swap(f1, f2);
+            swap(u, v);
+        }
+        u = ffather[f1]; f1 = head[u];
+    }
+    if(u == v) return u;
+    if(deep[u] > deep[v]) swap(u, v);
+    return u;
 }
-
-bool flag[maxn];
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int u, v, T, N, n, Case = 0;
-	cin >> T;
-	while(T --)
+	int root, cnt, to;
+	int T;
+	scanf("%d", &T);
+	for(int Casen = 1; Casen <= T; ++ Casen)
 	{
-		cin >> N;
-		init();
-		RESET(flag);
-		for(int i = 1;i <= N;i ++)
+		scanf("%d", &n);
+		eddy_tot = 0;
+		RESET(fir);
+		RESET(ans);
+		RESET(out);
+		for(int i = 1; i <= n; ++ i)
 		{
-			cin >> n;
-			u = i;
-			for(int j = 1;j <= n;j ++)
+			root = i;
+			scanf("%d", &cnt);
+			for(int j = 1; j <= cnt; ++ j)
 			{
-				cin >> v;
-				addedge(u, v);
-				addedge(v, u);
-				flag[v] = true;
+				scanf("%d", &to);
+				addedge(root, to);
+				addedge(to, root);
+				++ out[to];
 			}
 		}
-		int root;
-		for(int i = 1;i <= N;i ++)
-			if(! flag[i])
-			{
-				root = i;
-				break;
-			}
-		LCA_init(root, N);
-		cin >> n;
-		cout << "Case " << ++Case << ":" << endl;
-		while(n --)
+		for(int i = 1; i <= n; ++ i)
 		{
-			cin >> u >> v;
-			cout << query_LCA(u, v) << endl;
+			if(! out[i])
+			{
+				dfs1(i, 0, 0);
+				getpos(i, i);
+			}
+		}
+		printf("Case %d:\n", Casen);
+		int q;
+		scanf("%d", &q);
+		while(q --)
+		{
+			scanf("%d %d", &root, &to);
+			printf("%d\n", LCA(root, to));
 		}
 	}
 	return 0;
 }
-/*
-1
-7
-3 2 3 4
-0
-3 5 6 7
-0
-0
-0
-0
-2
-5 7
-2 7
-*/
-/*
-Case 1:
-3
-1
-*/
