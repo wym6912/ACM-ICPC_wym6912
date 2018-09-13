ff0704057a51893df905efb3051e3cb60bb24943
wym6912 <wym6912@outlook.com>
2018-09-13 21:23:21 +0800

    Upload UVA 11292 & 11300 & 11729

---
+++ UVA/11300.cpp
@@ -0,0 +1,140 @@
+//#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) ((_ << 1))
+# define DXB(_) ((_ << 1) | 1)
+
+# define next __Chtholly__
+# define x1 __Mercury__
+# define y1 __bbtl04__
+# define index __ikooo__
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 1000009;
+const int maxm = 300009;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#define cin.tie(0); cin.tie(nullptr);
+#define cout.tie(0); cout.tie(nullptr);
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+inline void out_number(ll x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+ll data[maxn], C[maxn];
+int n;
+
+int main()
+{
+	ios :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll sum, ave, mid, ans;
+	while(~ scanf("%d", &n))
+	{
+		sum = 0;
+		for(int i = 1; i <= n; ++ i) scanf("%lld", data + i), sum += data[i];
+		ave = sum / n;
+		C[0] = 0;
+		for(int i = 1; i < n; ++ i) C[i] = C[i - 1] + data[i] - ave;
+		sort(C, C + n);
+		mid = C[n >> 1], ans = 0;
+		for(int i = 0; i < n; ++ i) ans += abs(mid - C[i]);
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
+
