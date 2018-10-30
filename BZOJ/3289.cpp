27842260cf79039a72eff4c5342452465553277d
wym6912 <wym6912@outlook.com>
2018-10-30 21:45:11 +0800

    Update BZOJ/3289.cpp

---
+++ BZOJ/3289.cpp
@@ -0,0 +1,207 @@
+//#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) (((_) << 1))
+# define DXB(_) (((_) << 1) | 1)
+
+# define next __Chtholly__
+# define x1 __Mercury__
+# define y1 __bbtl04__
+# define index __ikooo__
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 50009;
+const int maxm = 65539;
+const ll inf = 1e18;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#define cin.tie(0); cin.tie(nullptr);
+#define cout.tie(0); cout.tie(nullptr);
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+template <class T>
+inline void out_number(T x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int n, m, blk, A[maxn];
+ll ans[maxn];
+
+ll tree[maxm];
+inline int lowbit(int x){return x & -x;}
+inline void update(int x, int val)
+{
+	for(int i = x; i < maxn; i += lowbit(i))
+		tree[i] += val;
+}
+inline ll sum(int x)
+{
+	if(! x) return 0;
+	ll ans = 0;
+	for(int i = x; i; i -= lowbit(i))
+		ans += tree[i];
+	return ans;
+}
+
+struct query
+{
+	int l, r, id;
+	query(int l = 1, int r = 1, int id = 1) : l(l), r(r), id(id) {}
+	bool operator < (const query &b) const
+	{
+		return l / blk == b.l / blk ? r < b.r : l / blk < b.l / blk;
+	}
+}qqq[maxn];
+
+vi g;
+
+inline void modui()
+{
+	ll tmp = 0;
+	int L = 1, R = 0;
+	for(int i = 1; i <= m; ++ i)
+	{
+		while(L < qqq[i].l)
+		{
+			update(A[L], -1);
+			tmp -= sum(A[L] - 1);
+			++ L;
+		}
+		while(L > qqq[i].l)
+		{
+			-- L;
+			update(A[L], 1);
+			tmp += sum(A[L] - 1);
+		}
+		while(R < qqq[i].r)
+		{
+			++ R;
+			update(A[R], 1);
+			tmp += R - L + 1 - sum(A[R]);
+		}
+		while(R > qqq[i].r)
+		{
+			update(A[R], -1);
+			tmp -= R - L - sum(A[R]);
+			-- R;
+		}
+		ans[qqq[i].id] = tmp;
+	}
+}
+
+int main()
+{
+	g.clear();
+	scanf("%d", &n);
+	for(register int i = 1; i <= n; ++ i) scanf("%d", A + i), g.push_back(A[i]);
+	sort(g.begin(), g.end());
+	g.erase(unique(g.begin(), g.end()), g.end());
+	for(register int i = 1; i <= n; ++ i) 
+		A[i] = lower_bound(g.begin(), g.end(), A[i]) - g.begin() + 1;	
+	scanf("%d", &m);
+	blk = (int)sqrt(1.0 * m);
+	for(int i = 1, f, t; i <= m; ++ i)
+	{
+		scanf("%d %d", &f, &t);
+		qqq[i] = query(f, t, i);
+	}
+	sort(qqq + 1, qqq + 1 + m);
+	modui();
+	for(int i = 1; i <= m; ++ i)
+		printf("%lld\n", ans[i]);
+	return 0;
+}
+
