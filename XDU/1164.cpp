8a65f559a9b115be7c61ed27b8a950099173f41b
wym6912 <wym6912@outlook.com>
2018-11-21 20:37:05 +0800

    Upload XDU 1164

---
+++ XDU/1164.cpp
@@ -0,0 +1,157 @@
+//#define NOSTDCPP
+//#define Cpp11
+//#define Linux_System
+#ifndef NOSTDCPP
+
+#include <bits/stdc++.h>
+
+#else
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <complex>
+#include <cstring>
+#include <cstdio>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#endif
+
+# ifdef Linux_System
+# define getchar getchar_unlocked
+# define putchar putchar_unlocked
+# endif
+
+# define RESET(_) memset(_, 0, sizeof(_))
+# define RESET_(_, val) memset(_, val, sizeof(_))
+# define fi first
+# define se second
+# define pb push_back
+# define mp make_pair
+# define midf(x, y) ((x + y) >> 1)
+# define DXA(_) (((_) << 1))
+# define DXB(_) (((_) << 1) | 1)
+
+# define next __Chtholly__
+# define x1 __Mercury__
+# define y1 __bbtl04__
+# define index __ikooo__
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector <int> vi;
+typedef set <int> si;
+typedef pair <int, int> pii;
+typedef long double ld;
+
+const int MOD = 1e9 + 7;
+const int maxn = 10000009;
+const int maxm = 300009;
+const ll inf = 1e18;
+const double pi = acos(-1.0);
+const double eps = 1e-6;
+
+ll myrand(ll mod){return ((ll)rand() << 32 ^ (ll)rand() << 16 ^ rand()) % mod;}
+
+template <class T>
+inline bool scan_d(T & ret)
+{
+	char c;
+	int sgn;
+	if(c = getchar(), c == EOF)return false;
+	while(c != '-' && (c < '0' || c > '9'))c = getchar();
+	sgn = (c == '-') ? -1 : 1;
+	ret = (c == '-') ? 0 : (c - '0');
+	while(c = getchar(), c >= '0' && c <= '9')
+		ret = ret * 10 + (c - '0');
+	ret *= sgn;
+	return true;
+}
+#ifdef Cpp11
+template <class T, class ... Args>
+inline bool scan_d(T & ret, Args & ... args)
+{
+	scan_d(ret);
+	scan_d(args...);
+}
+#define cin.tie(0); cin.tie(nullptr);
+#define cout.tie(0); cout.tie(nullptr);
+#endif
+inline bool scan_ch(char &ch)
+{
+	if(ch = getchar(), ch == EOF)return false;
+	while(ch == ' ' || ch == '\n')ch = getchar();
+	return true;
+}
+
+template <class T>
+inline void out_number(T x)
+{
+	if(x < 0)
+	{
+		putchar('-');
+		out_number(- x);
+		return ;
+	}
+	if(x > 9)out_number(x / 10);
+	putchar(x % 10 + '0');
+}
+
+int n, m;
+int A[maxn], fa[maxn], sum[maxn];
+bool v[maxn];
+
+ll dfs(int i)
+{
+	if(v[i]) return 0;
+	v[i] = true;
+	if(! fa[i])
+	{
+		sum[i] -= A[i]; //clear the initial point
+		return abs(sum[i]);
+	}
+	ll x = dfs(fa[i]); //goto the father place to clear the point 
+	A[i] += sum[fa[i]]; //clear the tag of this point
+	x += abs(A[i]);
+	sum[i] += sum[fa[i]] - A[i]; //clear the tag of now
+	return x;
+}
+
+int main()
+{
+	ll ans = 0;
+//	scanf("%d", &n);
+	scan_d(n);
+	fa[1] = 0;
+	for(register int i = 1, u, v; i < n; ++ i)
+	{
+		//scanf("%d %d", &u, &v);
+		scan_d(u);
+		scan_d(v);
+		fa[v] = u;
+	}
+	for(register int i = 1; i <= n; ++ i) scan_d(A[i]); //scanf("%d", A + i);
+	for(register int i = 1; i <= n; ++ i) ans += dfs(i);
+	printf("%lld\n", ans);
+	return 0;
+}
+
